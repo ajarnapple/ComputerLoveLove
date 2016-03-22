6d301586a5f0e910ba6978c4bd06123da566b197
masterUNG <ajarn_apple@hotmail.com>
2016-03-22 09:41:37 +0700

    Start

---
+++ app/src/main/java/appewtc/masterung/ungrestaurant/MainActivity.java
@@ -0,0 +1,68 @@
+package appewtc.masterung.ungrestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText userEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private String userString, passwordString;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Bind Widget การผูก
+        bindWidget();
+
+        //Button Controller ทำปุ่มให้ทำงาน
+        buttonController();
+
+    }   //Main Method นี่คือ Method หลัก Method ห่อหุ่ม Statement
+
+    private void buttonController() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Receive Value from Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                // Check Space
+                if (userString.equals("") || passwordString.equals("") ) {
+                    // Have Space
+                    MyAlertDialog myAlertDialog = new MyAlertDialog();
+                    myAlertDialog.myDialog(MainActivity.this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+
+                } else {
+
+                    //No Space
+                }
+
+
+            } //onClick
+        });
+
+    }   //buttonContraoller
+
+    private void bindWidget() {
+
+
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+        loginButton = (Button) findViewById(R.id.button);
+
+    }
+} //Main Class นี่ คือ class หลัก
